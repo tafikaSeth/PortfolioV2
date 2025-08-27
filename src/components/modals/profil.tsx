e5cce5980a4c075a92b2b15428375188338d7954
Seth TAFIKA <tafikaseth@gmail.com>
2025-08-27 09:39:53 +0300

    refact: responsive modal profile

--- src/components/modals/profil.tsx
+++ src/components/modals/profil.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 
 export function ModalProfil() {
     return (
-        <Avatar className="w-[50%] h-100">
+        <Avatar className="w-[50%] h-50 md:h-100">
             <AvatarImage src="/assets/images/profile.png" alt="profile" />
             <AvatarFallback>Profile</AvatarFallback>
         </Avatar>
