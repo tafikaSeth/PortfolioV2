3993aa327e780824ba4a90962d01d6454e59872e
Seth TAFIKA <tafikaseth@gmail.com>
2025-08-28 19:40:41 +0300

    feat: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.tsx'
 import Template from './template.tsx'
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Template>
       <App />
+      <Analytics />
     </Template>
   </StrictMode>,
 )
