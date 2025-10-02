e4d075ea38d970ab0777bff5f00f09731ac3ce72
Seth TAFIKA <tafikaseth@gmail.com>
2025-10-02 09:59:42 +0300

    remove the template.tsx from main

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.tsx'
-import Template from './template.tsx'
 import "../i18n.ts"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Template>
       <App />
       <Analytics />
-    </Template>
   </StrictMode>,
 )
